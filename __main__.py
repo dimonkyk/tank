cd2d8ce32903637a6fb11ef8e9c55e05361cb28a
dimonkyk <dimashedko2009@gmail.com>
2020-08-13 17:56:51 +0300

    file new

--- __main__.py
+++ __main__.py
@@ -1,32 +1,25 @@
-import pygame, tank
+import pygame, time, tank_player
 
-# создаем игру и окно
 pygame.init()
-pygame.mixer.init()  # для звука
-screen = pygame.display.set_mode((1000, 1000))
-pygame.display.set_caption("SurvivalTest")
-clock = pygame.time.Clock()
-group_sprite = pygame.sprite.Group()
-
-p = tank.Tank()
-group_sprite.add(p)
-
-
-# Цикл игры
-running = True
-while running:
-    clock.tick(60)
-
-    # Ввод процесса (события)
-    new_events = pygame.event.get()
-    for event in new_events:
-
-        # проверить закрытие окна
-        if event.type == pygame.QUIT:
-            running = False
-
-    screen.fill((10, 50, 100))
-    group_sprite.draw(screen)
-
-    # После отрисовки всего, переворачиваем экран
+s=0
+screen=pygame.display.set_mode([1000,500])
+tank=tank_player.Tank(0, 209)
+groopa=pygame.sprite.Group()
+groopa.add(tank,)
+while 1==1:
+    tank.move()
+    time.sleep(1/60)
+    a=pygame.event.get()
+    for d in a:
+        if d.type==12:
+            exit()
+        if d.type==pygame.KEYDOWN:
+            tank.speedx=1
+        if d.type==pygame.KEYUP:
+            tank.speedx=0
+    screen.fill([s,164,231])
+    if s<255:
+        s+=1
+    groopa.draw(screen)
     pygame.display.flip()
+
--- tank.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-
-class Tank(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-
-        self.image = pygame.Surface([20, 30])
-        self.rect = pygame.Rect(100, 100, 20, 30)
