343f9908d0142d58c343c750c619a582dfa7f6d2
dimonkyk <dimashedko2009@gmail.com>
2020-08-24 16:53:52 +0300

    новая версия что изменилось танк не выезжает за границу

--- __main__.py
+++ __main__.py
@@ -1,25 +1,42 @@
 import pygame, time, tank_player
 
 pygame.init()
-s=0
-screen=pygame.display.set_mode([1000,500])
-tank=tank_player.Tank(0, 209)
-groopa=pygame.sprite.Group()
-groopa.add(tank,)
-while 1==1:
+s = 0
+screen = pygame.display.set_mode([1000, 500])
+tank = tank_player.Tank(0, 209)
+groopa = pygame.sprite.Group()
+groopa.add(tank, )
+while 1 == 1:
     tank.move()
-    time.sleep(1/60)
-    a=pygame.event.get()
+    time.sleep(1 / 60)
+    a = pygame.event.get()
     for d in a:
-        if d.type==12:
+        if d.type == 12:
             exit()
-        if d.type==pygame.KEYDOWN:
-            tank.speedx=1
-        if d.type==pygame.KEYUP:
-            tank.speedx=0
-    screen.fill([s,164,231])
-    if s<255:
-        s+=1
+        if d.type == pygame.KEYDOWN and d.key == pygame.K_RIGHT:
+            tank.speedx = 1
+            tank.speedy = 0
+        if d.type == pygame.KEYUP and d.key == pygame.K_RIGHT:
+            tank.speedx = 0
+        if d.type == pygame.KEYDOWN and d.key == pygame.K_LEFT:
+            tank.speedx = -1
+            tank.speedy = 0
+        if d.type == pygame.KEYUP and d.key == pygame.K_LEFT:
+            tank.speedx = 0
+
+        if d.type == pygame.KEYDOWN and d.key == pygame.K_DOWN:
+            tank.speedy = 1
+            tank.speedx = 0
+        if d.type == pygame.KEYUP and d.key == pygame.K_DOWN:
+            tank.speedy = 0
+        if d.type == pygame.KEYDOWN and d.key == pygame.K_UP:
+            tank.speedy = -1
+            tank.speedx = 0
+        if d.type == pygame.KEYUP and d.key == pygame.K_UP:
+            tank.speedy = 0
+
+    screen.fill([s, 164, 231])
+    if s < 255:
+        s += 1
     groopa.draw(screen)
     pygame.display.flip()
-
--- tank_player.py
+++ tank_player.py
@@ -2,14 +2,25 @@ import pygame
 
 
 class Tank(pygame.sprite.Sprite):
-    def __init__(self, x, y,):
+    def __init__(self, x, y, ):
         pygame.sprite.Sprite.__init__(self)
         self.rect = pygame.Rect(x, y, 151, 82)
         self.image = pygame.image.load("C:/Users/H P/PycharmProjects/pygamea/navarot/kartinki/plta.png")
         self.speedx = 0
         self.speedy = 0
+        self.rect.left=500
 
     def move(self):
-        self.rect.right += self.speedx
         self.rect.top += self.speedy
+        self.rect.right += self.speedx
+        if self.rect.right>1000:
+            self.rect.right=1000
 
+        if self.rect.left<0:
+            self.rect.left=0
+
+        if self.rect.bottom > 500:
+            self.rect.bottom = 500
+
+        if self.rect.top < 0:
+            self.rect.top = 0
